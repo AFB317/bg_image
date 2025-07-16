ef44a5de5faa69068dde71d133e40fa4dedf6728
Alec Fedor Budomo <40199784+AFB317@users.noreply.github.com>
2025-07-16 22:21:21 +0200

    Update constants.dart

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,3 +2,4 @@ const String bgImage = "assets/bg_image.png";
 const String image1 = "assets/image1.png";
 const String image2 = "assets/image2.png";
 const String image3 = "assets/image3.png";
+const String image4 = "assets/image3.png";
